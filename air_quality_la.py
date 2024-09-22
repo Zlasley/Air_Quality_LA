096b5de8e04dec0990bf56451f2e9f07e61cd003
Zachary Lasley <116044492+Zlasley@users.noreply.github.com>
2024-09-21 19:42:31 -0500

    Update air_quality_la.py

--- air_quality_la.py
+++ air_quality_la.py
@@ -1,6 +1,6 @@
 """OpenAQ Air Quality Dashboard with Flask."""
 from flask import Flask
-import openaq0000000
+import openaq00000000
 from flask_sqlalchemy import SQLAlchemy
 
 APP = Flask(__name__)
